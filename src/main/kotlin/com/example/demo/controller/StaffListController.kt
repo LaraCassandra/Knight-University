9111e1b06678c508adf8a64039440d858b36a0bd
LaraCook <190218@virtualwindow.co.za>
2021-08-11 08:42:55 +0200

    Add more subject and staff data

--- src/main/kotlin/com/example/demo/controller/StaffListController.kt
+++ src/main/kotlin/com/example/demo/controller/StaffListController.kt
@@ -11,6 +11,9 @@ class StaffListController: Controller() {
         staffMembers.plusAssign(Staff("Adam Low", "8001120146083", "Admin", "NA", 0, 15000))
         staffMembers.plusAssign(Staff("Jesse Edwards", "7903020125071", "Lecturer", "IXT, VC", 16, 6400))
         staffMembers.plusAssign(Staff("Nelly Lavigne", "8409270183045", "Lecturer", "RP, IDV", 20, 8000))
+        staffMembers.plusAssign(Staff("Emilia Johnson", "9207301205369", "Lecturer", "CC", 8, 3200))
+        staffMembers.plusAssign(Staff("Jay Wilson", "8512250192087", "Lecturer", "IOT", 8, 3200))
+        staffMembers.plusAssign(Staff("Victor Brown", "9501019256075", "Lecturer", "IOS", 8, 3200))
 
     }
 }
--- src/main/kotlin/com/example/demo/controller/SubjectListController.kt
+++ src/main/kotlin/com/example/demo/controller/SubjectListController.kt
@@ -11,7 +11,11 @@ class SubjectListController: Controller() {
     init {
         subjects.plusAssign(Subject("Interactive Development", "IDV", "Nelly Lavigne", 20, 16, 1200, "Lara Cook, John Doe, Marcus Dean"))
         subjects.plusAssign(Subject("Research Practice", "RP", "Nelly Lavigne", 20, 4, 1000, "Lara Cook, John Doe, Jane Smith, Marcus Dean"))
-        subjects.plusAssign(Subject("Visual Culture", "VC", "Jesse Edwards", 20, 8, 1200, "Lara Cook, Jane Doe, Jane Smith, Marcus Dean"))
+        subjects.plusAssign(Subject("Visual Culture", "VC", "Jesse Edwards", 20, 8, 1200, "Lara Cook, John Doe, Jane Smith, Marcus Dean"))
+        subjects.plusAssign(Subject("Interaction Theory", "IXT", "Jesse Edwards", 20, 4, 1000, "Lara Cook, Jane Doe, Jane Smith, Marcus Dean"))
+        subjects.plusAssign(Subject("Smart Objects", "CC", "Emilia Johnson", 20, 8, 1000, "Lara Cook, John Doe"))
+        subjects.plusAssign(Subject("Internet of Things", "IOT", "Jay Wilson", 20, 8, 1000, "Lara Cook, Marcus Dean"))
+        subjects.plusAssign(Subject("iOS", "IOS", "Victor Brown", 20, 8, 1000, "John Doe"))
 
     }
 }
